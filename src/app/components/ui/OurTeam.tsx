f53aed42c1cd2634f0aa4630232b540af87c7a62
An <anthunguyen03@gmail.com>
2026-02-06 13:46:51 -0800

    Add spacing and gap with responsive grid col for desktop

--- src/app/components/ui/OurTeam.tsx
+++ src/app/components/ui/OurTeam.tsx
@@ -4,10 +4,10 @@ import HeadshotCard from "./cards/HeadshotCard";
 export default function OurTeam() {
   return (
     <div className="mt-3xl mr-xl mb-3xl ml-xl">
-      <div className="gap-2xl pb-lg">
-        <h1 className="text-accent-sm text-cyan-400">Meet The Organizers</h1>
+      <div className="flex flex-col gap-lg pb-lg">
+        <h1 className="text-acc ent-sm text-cyan-400">Meet The Organizers</h1>
         <h2 className="text-heading-lg text-neutral-100">Our Team</h2>
-        <p className="text-body-sm text-neutral-100">
+        <p className="text-body-sm text-neutral-100 mb-2xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque
           tortor, hendrerit ut eleifend nec, aliquet sed nibh. Suspendisse
           potenti. Nulla eu libero quam. Pellentesque faucibus ultricies quam,
@@ -15,7 +15,7 @@ export default function OurTeam() {
         </p>
       </div>
 
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-2xl mb-2xl">
         <HeadshotCard
           role="Co-Chair"
           name="Jennifer Huang"
@@ -26,7 +26,9 @@ export default function OurTeam() {
           name="Sydney Komanac"
           bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque tortor, hendrerit ut eleifend nec, aliquet sed nibh. Suspendisse potenti. Nulla eu libero quam. Pellentesque faucibus ultricies quam, eget cursus velit porta sit amet. Aenean sollicitudin lacus mauris."
         ></HeadshotCard>
+      </div>
 
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2xl mb-2xl">
         <HeadshotCard
           role="Finance & Sponsorship"
           name="Mara"
@@ -42,7 +44,9 @@ export default function OurTeam() {
           name="Tina"
           bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque tortor, hendrerit ut eleifend nec, aliquet sed nibh. Suspendisse potenti. Nulla eu libero quam. Pellentesque faucibus ultricies quam, eget cursus velit porta sit amet. Aenean sollicitudin lacus mauris."
         ></HeadshotCard>
+      </div>
 
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2xl mb-2xl">
         <HeadshotCard
           role="Operations & Logistics"
           name="Serena"
@@ -58,7 +62,9 @@ export default function OurTeam() {
           name="Laraib"
           bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque tortor, hendrerit ut eleifend nec, aliquet sed nibh. Suspendisse potenti. Nulla eu libero quam. Pellentesque faucibus ultricies quam, eget cursus velit porta sit amet. Aenean sollicitudin lacus mauris."
         ></HeadshotCard>
+      </div>
 
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2xl mb-2xl">
         <HeadshotCard
           role="Recruitment"
           name="Felicia"
@@ -74,7 +80,9 @@ export default function OurTeam() {
           name="Julia"
           bio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla neque tortor, hendrerit ut eleifend nec, aliquet sed nibh. Suspendisse potenti. Nulla eu libero quam. Pellentesque faucibus ultricies quam, eget cursus velit porta sit amet. Aenean sollicitudin lacus mauris."
         ></HeadshotCard>
+      </div>
 
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2xl mb-2xl">
         <HeadshotCard
           role="Visual Design & Marketing"
           name="Bianca"
