8c4579c54fd5c7bb2f0a01e55221cbb28b02b2d6
An <anthunguyen03@gmail.com>
2026-02-22 18:24:32 -0800

    style: adjust text position for Mascot Description
    TO-DO: Adjust paw position

--- src/components/ui/cards/MascotCard.tsx
+++ src/components/ui/cards/MascotCard.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Image from "next/image";
 
 interface MascotProps {
   name: string;
@@ -22,23 +23,34 @@ function MascotCard({
   namePos,
 }: MascotProps) {
   return (
-    <section className="w-full py-xl rounded-xl bg-neutral-700 overflow-hidden">
-      <div className="flex flex-col items-center text-center mx-auto max-w-5xl">
-        <div
-          className={clsx("text-jocky-normal rotate-[-7.461deg]", nameColour)}
-        >
+    <section className="w-full rounded-xl bg-neutral-700 overflow-hidden">
+      <div className="flex flex-col items-center text-center mx-auto max-w-5xl gap-lg">
+        <div className={clsx("text-jocky-normal", nameColour, namePos)}>
           {name}
         </div>
-        <div className="relative inline-block w-[251.427px] h-[220.429px] aspect-auto">
-          <img className="" src={mascotImg} alt={`${name}-img`}></img>
-          <img
-            className="absolute w-[145.457px] h-[128.499px] left-1/2 top-1/2 rotate-[17.884deg] "
-            src={pawImg}
-            alt={`${name}-paw`}
-          ></img>
+        <div className="relative inline-block w-[251.427px] h-[220.429px]">
+          <Image
+            className={clsx("w-full h-full object-contain", mascotPos)}
+            src={mascotImg}
+            alt={`${name}-img`}
+            fill
+          />
+          <div
+            className={clsx(
+              "absolute right-0 bottom-0 w-[120px] h-[120px] pointer-events-none",
+              pawPos,
+            )}
+          >
+            <Image
+              src={pawImg}
+              alt={`${name}-paw`}
+              fill
+              className="object-contain"
+            />
+          </div>
         </div>
 
-        <p className="w-full text-white text-body-mascot px-3xl">
+        <p className="w-full text-white text-body-mascot px-3xl pb-[21px]">
           {description}
         </p>
       </div>
