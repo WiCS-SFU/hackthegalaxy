263fc2daa8b5a301c5c4678d61dad2c6afe0a502
bei <sydneybeik@icloud.com>
2026-02-23 14:50:17 -0800

    Fixed background gradient and route for back to home button

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -11,7 +11,7 @@ export default function QuizPage() {
         <section className="relative min-h-screen bg-neutral-800 overflow-hidden">
             <div>
                 <div className="absolute z-0 w-[772.06px] h-[772.06px] 
-                    left-[-300px] top-[440px]
+                    left-[-300px] top-[460px]
                     bg-[radial-gradient(ellipse_72.73%_75.28%_at_67.98%_65.87%,_var(--pink-100,_#F7F0FB)_0%,_var(--pink-400,_#BE89E2)_23%,_var(--purple-600,_#4119B8)_47%,_var(--purple-800,_#16064A)_77%,_var(--neutral-800,_#171621)_100%)] 
                     rounded-full blur-[53.84px]"/>
 
@@ -46,7 +46,7 @@ export default function QuizPage() {
                     </div>
 
                     <div className="z-10">
-                            <Button onClick={() => router.push("/quiz")} variant="outline" size="large" iconPos="none" className="w-[181px] justify-center">
+                            <Button onClick={() => router.push("/")} variant="outline" size="large" iconPos="none" className="w-[181px] justify-center">
                                 Go back to home
                             </Button>
                     </div>
