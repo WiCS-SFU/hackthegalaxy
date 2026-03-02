dc201dce8b133290600eb935d50489da27f8c3ba
An <anthunguyen03@gmail.com>
2026-03-01 16:58:14 -0800

    feat(ui): Add Ghost Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,14 +1,40 @@
 /**
- * EXAMPLE TO USE THE BUTTON:
- *  <Button variant="default" icon={<PlusIcon className="h-5 w-5" />} size="large" iconPos="left">
-      text go here
-    </Button>
-
-    OR USE FOR DISABLE:
-    <Button disabled icon={<PlusIcon className="h-5 w-5" />} size="large" iconPos="left">
-      text go here
-    </Button>
+ * Button Component:-------------------------------------------
+ *
+ * Props:
+ * - variant: "default" | "ghost" | "outline"
+ * - size: "default" | "large" | "small"
+ * - iconPos: "none" | "left" | "right"
+ * - icon?: React node (optional)
+ * - disabled?: boolean
+ *
+ * Default Button with <Button/>:-------------------------------
+ * variant = "default"
+ * size = "default"
+ * iconPos = "none"
+ *
+ * EXAMPLE:------------------------------------------------------
+ * <Button
+ *   variant="default"
+ *   size="large"
+ *   icon={<PlusIcon className="h-5 w-5" />}
+ *   iconPos="left"
+ * >
+ *   Apply Now
+ * </Button>
+ *
+ * DISABLE EXAMPLE:-----------------------------------------------
+ * <Button disabled>
+ *   Apply Now
+ * </Button>
+ *
+ * COMMON USAGE:---------------------------------------------------
+ * Default: <Button>Apply Now</Button>
+ * Outline: <Button variant="outline">Apply Now</Button>
+ * Ghost: <Button variant="ghost">Apply Now</Button>
+ * Icon button: <Button icon={<PlusIcon />} iconPos="left">Apply Now</Button> --> Remember to import PlusIcon from Heroicons
  */
+
 import { forwardRef } from "react";
 import React from "react";
 import { cva, VariantProps } from "class-variance-authority";
@@ -21,6 +47,7 @@ const base = "inline-flex items-center rounded-md transition";
 const buttonStyles = cva(base, {
   variants: {
     variant: {
+      ghost: "text-pink-100",
       default:
         "bg-pink-100 text-purple-600 hover:border-none hover:bg-purple-600 hover:text-pink-100",
       outline: "text-pink-100 border-1 border-pink-100",
@@ -36,12 +63,36 @@ const buttonStyles = cva(base, {
       right: "flex flex-row-reverse",
     },
     disabled: {
-      true: "bg-pink-100 text-purple-300 pointer-events-none",
+      true: "pointer-events-none",
       false: "",
     },
   },
 
   compoundVariants: [
+    //DEFAULT hover
+    {
+      variant: "default",
+      disabled: false,
+      class: "hover:bg-purple-600 hover:text-pink-100",
+    },
+    //DEFAULT disabled
+    {
+      variant: "default",
+      disabled: true,
+      class: "bg-pink-100 text-purple-300",
+    },
+    //Ghost hover
+    {
+      variant: "ghost",
+      disabled: false,
+      class: "hover:bg-purple-600 hover:text-pink-100",
+    },
+    //Ghost disabled
+    {
+      variant: "ghost",
+      disabled: true,
+      class: "text-purple-300",
+    },
     //Outline hover
     {
       variant: "outline",
