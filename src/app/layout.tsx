5fbb0add512f92cd93ae8c0913549e96187d1f18
An <anthunguyen03@gmail.com>
2026-03-02 16:58:12 -0800

    chore: fix duplicate jockyStarline font in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,12 +13,6 @@ const jockyStarline = localFont({
   display: "swap",
 });
 
-const jockyStarline = localFont({
-  src: "./fonts/JockyStarline.ttf",
-  variable: "--font-jocky",
-  display: "swap",
-});
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
